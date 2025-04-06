034c0d137a5c980b44f06a0b4488836f1c6e5f0a
ruandejuxian <ruanyujian@set.edu.vn>
2025-04-06 20:51:24 +0700

    fix worker link

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ const CONFIG = {
   
   // Cloudflare Worker settings
   cloudflare: {
-    proxyUrl: 'https://muonmaugaixinh.karrythaonguyen0.workers.dev/'
+    proxyUrl: 'https://vngirls.karrythaonguyen0.workers.dev/'
   },
   
   // Google Sheets API settings
